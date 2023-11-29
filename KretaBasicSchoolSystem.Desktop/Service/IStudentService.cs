3de0da67e83cbe6971be25c71419b3f2029d0fb3
Csaba Gyuris <gyuris.csaba@vasvari.org>
2023-11-29 09:47:57 +0100

    Elkészítjük a megfelelő mappába az interface-t és az osztályt is

---
+++ KretaBasicSchoolSystem.Desktop/Service/IStudentService.cs
@@ -0,0 +1,11 @@
+﻿using KretaBasicSchoolSystem.Desktop.Models;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace KretaBasicSchoolSystem.Desktop.Service
+{
+    public interface IStudentService
+    {
+        public Task<List<Student>> SelectAllStudent();
+    }
+}
---
+++ KretaBasicSchoolSystem.Desktop/Service/StudentService.cs
@@ -0,0 +1,14 @@
+﻿using KretaBasicSchoolSystem.Desktop.Models;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace KretaBasicSchoolSystem.Desktop.Service
+{
+    public class StudentService : IStudentService
+    {
+        public Task<List<Student>> SelectAllStudent()
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
