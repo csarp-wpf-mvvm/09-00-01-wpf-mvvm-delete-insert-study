83c658ccf8d7db8b8b2166c0e6406091ee97e1e5
Csaba Gyuris <gyuris.csaba@vasvari.org>
2023-11-29 09:55:10 +0100

    Injektáljuk a service-be a http clienst

--- KretaBasicSchoolSystem.Desktop/Service/StudentService.cs
+++ KretaBasicSchoolSystem.Desktop/Service/StudentService.cs
@@ -1,11 +1,19 @@
 ﻿using KretaBasicSchoolSystem.Desktop.Models;
 using System.Collections.Generic;
+using System.Net.Http;
 using System.Threading.Tasks;
 
 namespace KretaBasicSchoolSystem.Desktop.Service
 {
     public class StudentService : IStudentService
     {
+        private readonly HttpClient _httpClient;
+
+        public StudentService(IHttpClientFactory httpClientFactory)
+        {
+            _httpClient = httpClientFactory.CreateClient("KretaApi");
+        }
+
         public Task<List<Student>> SelectAllStudent()
         {
             throw new System.NotImplementedException();
