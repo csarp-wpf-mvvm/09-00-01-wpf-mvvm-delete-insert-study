ca6c520ecf15c126a2ddb26599e50fe7386385ca
Csaba Gyuris <gyuris.csaba@vasvari.org>
2023-11-29 09:28:30 +0100

    Backend

---
+++ Kreata.Backend/Context/ModelBuilderExtension.cs
@@ -0,0 +1,39 @@
+﻿using Kreata.Backend.Datas.Entities;
+using Kreata.Backend.Datas.Enums;
+using Microsoft.EntityFrameworkCore;
+
+namespace Kreata.Backend.Context
+{
+    public static class ModelBuilderExtension
+    {
+        public static void Seed(this ModelBuilder modelBuilder)
+        {
+            List<Student> students = new List<Student>
+            {
+                new Student
+                {
+                    Id=Guid.NewGuid(),
+                    FirstName="János",
+                    LastName="Jegy",
+                    BirthsDay=new DateTime(2022,10,10),
+                    SchoolYear=9,
+                    SchoolClass = SchoolClassType.ClassA,
+                    EducationLevel="érettségi"
+                },
+                new Student
+                {
+                    Id=Guid.NewGuid(),
+                    FirstName="Szonja",
+                    LastName="Stréber",
+                    BirthsDay=new DateTime(2021,4,4),
+                    SchoolYear=10,
+                    SchoolClass = SchoolClassType.ClassB,
+                    EducationLevel="érettségi"
+                }
+            };
+
+            // Students
+            modelBuilder.Entity<Student>().HasData(students);
+        }
+    }
+}
