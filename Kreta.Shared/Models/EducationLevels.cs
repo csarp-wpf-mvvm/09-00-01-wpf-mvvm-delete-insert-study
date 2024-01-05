850b1338ac1e26450b5821be7cc66cde222746ad
Csaba Gyuris <gyuris.csaba@vasvari.org>
2024-01-05 13:25:42 +0100

    Osztályok kódjának átmásolása

--- Kreta.Shared/Models/EducationLevels.cs
+++ Kreta.Shared/Models/EducationLevels.cs
@@ -1,12 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Kreta.Shared.Models
+﻿namespace Kreta.Shared.Models
 {
-    internal class EducationLevels
+    public class EducationLevels
     {
+        public List<string> AllEducationLevels { get; } = new List<string> { "érettségi", "szakmai érettségi", "szakmai vizsga" };
     }
 }
