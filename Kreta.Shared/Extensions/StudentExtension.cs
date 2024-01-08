246105fbb9c217c5b91afd565c2ff164c96e599e
Csaba Gyuris <gyuris.csaba@vasvari.org>
2024-01-08 12:03:38 +0100

    ToStudentDto és ToStudent statikus kiterjesztett metódusok

---
+++ Kreta.Shared/Extensions/StudentExtension.cs
@@ -0,0 +1,37 @@
+﻿using Kreta.Shared.Dtos;
+using Kreta.Shared.Models;
+
+namespace Kreta.Shared.Extensions
+{
+    public static class StudentExtension
+    {
+        public static StudentDto ToStudentDto(this Student student)
+        {
+            return new StudentDto
+            {
+                Id=student.Id,
+                FirstName = student.FirstName,
+                LastName = student.LastName,
+                BirthsDay = student.BirthsDay,
+                SchoolYear = student.SchoolYear,
+                SchoolClass = student.SchoolClass,
+                EducationLevel = student.EducationLevel
+            };
+        }
+
+        public static Student ToStudent(this StudentDto studentdto) 
+        {
+            return new Student
+            {
+                Id = studentdto.Id,
+                FirstName = studentdto.FirstName,
+                LastName = studentdto.LastName,
+                BirthsDay = studentdto.BirthsDay,
+                SchoolYear = studentdto.SchoolYear,
+                SchoolClass = studentdto.SchoolClass,
+                EducationLevel = studentdto.EducationLevel
+            };
+        }
+    }
+
+}
