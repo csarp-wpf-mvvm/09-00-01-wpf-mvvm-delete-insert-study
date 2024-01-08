5d63a079385dac5a329644fc23e0a90bab8aa307
Csaba Gyuris <gyuris.csaba@vasvari.org>
2024-01-08 13:21:04 +0100

    StudentViewModel rekatoráció a ViewModel rétegben

--- KretaBasicSchoolSystem.Desktop/ViewModels/SchoolCitizens/StudentViewModel.cs
+++ KretaBasicSchoolSystem.Desktop/ViewModels/SchoolCitizens/StudentViewModel.cs
@@ -1,6 +1,6 @@
 ﻿using CommunityToolkit.Mvvm.ComponentModel;
 using CommunityToolkit.Mvvm.Input;
-using KretaBasicSchoolSystem.Desktop.Models;
+using Kreta.Shared.Models;
 using KretaBasicSchoolSystem.Desktop.Service;
 using KretaBasicSchoolSystem.Desktop.ViewModels.Base;
 using System.Collections.Generic;
